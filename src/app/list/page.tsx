fd3f5bbeb81d6c99dd917b5779fba283b63a64ea
bsWiwat <bs.wiwat@gmail.com>
2025-02-23 16:54:53 +0700

    fixed(): config list page

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import CategoryList from "@/components/CategoryList";
+import Filter from "@/components/Filter";
 import ProductList from "@/components/ProductList";
 import Slider from "@/components/Slider";
 
@@ -11,8 +11,10 @@ interface Product {
   price: number;
   currency: string;
 }
-const HomePage = () => {
+
+const ListPage = () => {
   const [products, setProducts] = useState<Product[]>([]); // กำหนดชนิดของ products เป็น array ของ Product
+
   useEffect(() => {
     const fetchProducts = async () => {
       const res = await fetch("/data/products.json");
@@ -23,44 +25,23 @@ const HomePage = () => {
   }, []);
   return (
     <>
-      <div className="">
-        {/* Baner Silder*/}
+      <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+        {/* CAMPAIGN */}
         <Slider />
-
-        {/*Categorylist*/}
-        <div className="mt-10">
-          <CategoryList />
-        </div>
-
-        {/*Recoment Product Card list*/}
-        <div className="mt-16 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-32 ">
-          <div className="flex flex-row justify-between">
-            <h1 className="text-2xl mb-6 font-bold">Recommend</h1>
-            <button className="text-sm md:text-lg text-white bg-[#D99F2B] px-10 h-9">
-              See all
-            </button>
-          </div>
-
-          <div className="flex flex-wrap gap-10 justify-center mx-5 ">
-            {/* map ข้อมูลแต่ละ product และส่งไปยัง Card component */}
-            {products.slice(0, 4).map((product) => (
-              <ProductList key={product.id} product={product} />
-            ))}
-          </div>
-        </div>
-
-        <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-32">
-          <h1 className="text-2xl font-bold mb-6">New Products</h1>
-          <div className="flex flex-wrap gap-10 justify-center mx-5">
-            {/* map ข้อมูลแต่ละ product และส่งไปยัง Card component */}
-            {products.slice(0, 4).map((product) => (
-              <ProductList key={product.id} product={product} />
-            ))}
-          </div>
-        </div>
+        {/* Filter */}
+        <Filter />
+        {/* Products */}
+        <h1 className="mt-12 text-xl font-semibold">For you!</h1>
+        {products.slice(0, 4).map((product) => (
+          <ProductList key={product.id} product={product} />
+        ))}
       </div>
     </>
   );
 };
 
-export default HomePage;
+export default ListPage;
+
+
+
+
